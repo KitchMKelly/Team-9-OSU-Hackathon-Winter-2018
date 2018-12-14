073477f42364185529cd034c6667e9912c896342
Herbert Diaz <hrbrtdiaz@gmail.com>
2018-12-14 09:44:49 -0600

    Added the Cart Class header and implementation files: Cart.hpp + Cart.cpp. Added some code to Cart.hpp but NOT Cart.cpp yet

---
+++ Cart.cpp
@@ -0,0 +1,9 @@
+/******************************************************************************
+** This is the implementation file for the Cart class.
+** The Cart class holds the total price of the items in the Cart, the number of
+** items in the cart, and all the titles inside of the Cart. This class also
+** has methods to set/get all the Data members, as well as print the item
+** within the cart.
+******************************************************************************/
+// Cart.cpp
+
