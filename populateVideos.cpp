5b392d69139c66dc0ade138065306bb3e8e76dcf
Jessica Albert <jessmalbert0807@gmail.com>
2018-12-14 09:24:25 -0600

    Test print all movie info from excel

--- populateVideos.cpp
+++ populateVideos.cpp
@@ -2,10 +2,13 @@
 #include <fstream>
 #include <string>
 
-using namespace std;
+using std::cout;
+using std::endl;
+using std::string;
+
 
 string count;
-int numLines = 0;
+int numLines = 0; //to compare output with # excel lines
 
 string title;
 string genre;
@@ -14,22 +17,25 @@ string actor;
 
 int main()
 {
-	ifstream movie_data("movie-dataset.csv");
+	std::ifstream movie_data("movie-dataset.csv");
 
-		if (!movie_data)
+	if (!movie_data)
 		cout << "Error: The file did not open" << endl;
 	else
 	{
-		while(getline(movie_data, count))
+		while(movie_data.good())
 		{
                 	numLines += 1;
                 	getline(movie_data, title, ',');
                         getline(movie_data, genre, ',');
                         getline(movie_data, director, ',');
-                        getline(movie_data, actor, ','); 
-		cout << "total movies is" << numLines << endl;
+                        getline(movie_data, actor, '\n'); 
+		
+			cout << "Title: " << title << ", Genre: " << genre << ", Director: " << director << ", Main Actor/Actress: " << actor << endl;  
+                cout << "total movies is" << numLines << endl;
+		}
 		movie_data.close();
-	}
+        }
 
 	return 0;
 }
